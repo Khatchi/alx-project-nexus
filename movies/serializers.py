5ad46aab1db1134e78fcc2e0452d9838afb0170d
Khatchi <solomon9ice2014@gmail.com>
2025-07-31 14:44:05 +0100

    Add UserSerializer for the User model.

---
+++ movies/serializers.py
@@ -0,0 +1,28 @@
+from rest_framework import serializers
+from movies.models import User, Movie, Rating, Watchlist, TrendingMovie, Recommendation
+
+class UserSerializer(serializers.ModelSerializer):
+    """
+    Serializer for the User model.
+    """
+
+    class Meta:
+        """Meta options for the UserSerializer."""
+
+        model = User
+
+        fields = [
+            'user_id',
+            'username',
+            'email',
+            'first_name',
+            'last_name',
+            'phone_number',
+            'preferences',
+            'is_active',
+            'date_joined',
+            'created_at',
+            'updated_at'
+        ]
+        read_only_fields = ['user_id', 'date_joined', 'created_at', 'updated_at']
+        
